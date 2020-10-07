3428420719f3fffb08434f992582dd06064b04a5
kotlyarDG <kotlyar.d.g@gmail.com>
2020-10-07 16:44:30 +0300

    anim

--- js/script.js
+++ js/script.js
@@ -249,4 +249,17 @@ $(document).ready(function () {
 		autoplaySpeed: 2000,
 		swipe: false
 	});
+
+	// Функция для скролла по ссылке-якорю
+	// вместо class-link - реальный класс ссылки-якоря
+	// вместо slow - необходимую скорость
+	$(document).on('click', '.class-link', function (e) {
+		var linkID = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(linkID).offset().top
+		}, 'slow');
+		e.preventDefault();
+	});
+
+	AOS.init();
 });	
