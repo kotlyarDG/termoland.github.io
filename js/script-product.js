fbb517480fe9702c813e4b3f5e76666f25f3f6bd
kotlyarDG <kotlyar.d.g@gmail.com>
2020-10-30 11:24:29 +0300

    fix

--- js/script-product.js
+++ js/script-product.js
@@ -142,4 +142,99 @@ $(document).ready(function () {
 		src = $(this).data('image');
 		image.attr("src", src);
 	});
+
+	const popupLinks = document.querySelectorAll('.popup-link');
+	const body = document.querySelector('body');
+
+	let unlock = true;
+
+	// timeout - время анимации в css
+	const timeout = 880;
+
+	if (popupLinks.length > 0) {
+		for (let index = 0; index < popupLinks.length; index++) {
+			const popupLink = popupLinks[index];
+			popupLink.addEventListener("click", function (e) {
+				const popupName = popupLink.getAttribute('href').replace('#', '');
+				const currentPopup = document.getElementById(popupName);
+				popupOpen(currentPopup);
+				e.preventDefault();
+			});
+		}
+	}
+
+	const popupCloseIcon = document.querySelectorAll('.close-popup');
+	if (popupCloseIcon.length > 0) {
+		for (let index = 0; index < popupCloseIcon.length; index++) {
+			const el = popupCloseIcon[index];
+			el.addEventListener('click', function (e) {
+				popupClose(el.closest('.popup'));
+				e.preventDefault();
+			});
+		}
+	}
+
+	function popupOpen(currentPopup) {
+		if (currentPopup && unlock) {
+			const popupActive = document.querySelector('.popup.open');
+			if (popupActive) {
+				popupClose(popupActive, false);
+			} else {
+				bodyLock();
+			}
+			currentPopup.classList.add('open');
+			currentPopup.addEventListener('click', function (e) {
+				if (!e.target.closest('.popup__content')) {
+					popupClose(e.target.closest('.popup'));
+				}
+			});
+		}
+	}
+
+	function popupClose(popupActive, doUnlock = true) {
+		if (unlock) {
+			popupActive.classList.remove('open');
+			body.classList.remove('_lock');
+
+		}
+	}
+
+
+	function bodyLock() {
+
+		body.classList.add('_lock');
+
+	}
+
+	document.addEventListener('keydown', function (e) {
+		if (e.which === 27) {
+			const popupActive = document.querySelector('.popup.open');
+			popupClose(popupActive);
+		}
+	});
+
+	(function () {
+		// проверяем поддержку
+		if (!Element.prototype.closest) {
+			// реализуем
+			Element.prototype.closest = function (css) {
+				var node = this;
+				while (node) {
+					if (node.matches(css)) return node;
+					else node = node.parentElement;
+				}
+				return null;
+			};
+		}
+	})();
+	(function () {
+		// проверяем поддержку
+		if (!Element.prototype.matches) {
+			// определяем свойства
+			Element.prototype.matches = Element.prototype.matchesSelector ||
+				Element.prototype.webkitMatchesSelector ||
+				Element.prototype.mozMatchesSelector ||
+				Element.prototype.msMatchesSelector;
+		}
+	})();
 });
